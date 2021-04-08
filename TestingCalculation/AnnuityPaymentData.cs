3f69cb8352b7f532998dc18c312f03607a79f529
Garn87 <Garn87@list.ru>
2021-04-09 02:28:07 +0500

    finishedTest

--- TestingCalculation/AnnuityPaymentData.cs
+++ TestingCalculation/AnnuityPaymentData.cs
@@ -13,23 +13,53 @@ namespace TestingCalculation
             creditAmount = model.creditAmount;
             percentRate = model.percentRate;
             numberOfMonths = model.numberOfMonths;
+
+            balanceOfDebt = model.creditAmount;
+            annuityRate = AnnuityRate();
         }
 
         private decimal AnnuityRate()
         {
             decimal annuityRateValue;
-            decimal _percentDoub = percentRate.ByMonth().ToDouble();
+            decimal _percentDoub = percentRate.ByMonth().PercentNumerical();
             decimal _divider = divider();
             decimal divider()
             {
                 return
                     (decimal)(Math.Pow(1 +
-                    (double)percentRate.ByMonth().ToDouble(),
+                    (double)percentRate.ByMonth().PercentNumerical(),
                     (double)numberOfMonths) - 1);
             }
             annuityRateValue = creditAmount * (_percentDoub+(_percentDoub / _divider));
-            return annuityRateValue.Round(1);
+            return annuityRateValue.Round(4);
         }
+
+        private decimal balanceOfDebt { get; set; }
+        private decimal percentOnDebt { get; set; }
+        private decimal mainDebt { get; set; }
+        private decimal annuityRate { get; set; }
+
+        private void PercentOnDebt() =>
+            percentOnDebt = (balanceOfDebt* (percentRate.PercentNumerical().ByMonth())).Round(4);
+        private void MainDebt() =>
+            mainDebt = annuityRate - percentOnDebt;
+        private void BalanceOfDebt() =>
+            balanceOfDebt = balanceOfDebt - mainDebt;
+
+
+        public ViewModel ViewData()
+        {
+            PercentOnDebt();
+            MainDebt();
+            BalanceOfDebt();
+            return new ViewModel(
+                balanceOfDebt,
+                percentOnDebt,
+                mainDebt,
+                annuityRate
+                );
+        }
+
     }
 
 }
--- TestingCalculation/Converting.cs
+++ TestingCalculation/Converting.cs
@@ -7,7 +7,7 @@ namespace TestingCalculation
     public static class Converting
     {
         public static decimal ByMonth(this decimal percent) => (percent / 12);
-        public static decimal ToDouble(this decimal percent) => (percent / 100);
+        public static decimal PercentNumerical(this decimal percent) => (percent / 100);
         public static decimal Round(this decimal doub, int i) => Math.Round(doub, i);
 
     }
