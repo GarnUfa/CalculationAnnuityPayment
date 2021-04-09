294821de731c09a689d87be0e1ca508968ddbc81
Garn87 <Garn87@list.ru>
2021-04-09 16:06:44 +0500

    firstsettings

---
+++ CalculationAnnuityPayment/Controllers/AnnuityPayment.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculationAnnuityPayment.Controllers
+{
+    public class AnnuityPayment : Controller
+    {
+        public IActionResult CreditData()
+        {
+            return View();
+        }
+        public IActionResult CalculationResults()
+        {
+            return View();
+        }
+        
+    }
+}
