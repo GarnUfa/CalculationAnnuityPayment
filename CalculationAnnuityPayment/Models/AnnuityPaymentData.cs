ecc30b41b4f172b76fefdb64e8d0f3d502e9381d
Garn87 <Garn87@list.ru>
2021-04-14 00:14:23 +0500

    addconverting

--- CalculationAnnuityPayment/Models/AnnuityPaymentData.cs
+++ CalculationAnnuityPayment/Models/AnnuityPaymentData.cs
@@ -1,13 +1,65 @@
-﻿namespace TestingCalculation
+﻿using System;
+
+namespace TestingCalculation
 {
     internal class AnnuityPaymentData
     {
-        private AnnuityPaymentModel model;
-
+        private decimal creditAmount { get; set; }
+        private decimal percentRate { get; set; }
+        private decimal numberOfMonths { get; set; }
 
         public AnnuityPaymentData(AnnuityPaymentModel model)
         {
-            this.model = model;
+            creditAmount = model.creditAmount;
+            percentRate = model.percentRate;
+            numberOfMonths = model.numberOfMonths;
+
+            balanceOfDebt = model.creditAmount;
+            annuityRate = AnnuityRate();
         }
+
+        private decimal AnnuityRate()
+        {
+            decimal annuityRateValue;
+            decimal _percentDoub = percentRate.ByMonth().PercentNumerical();
+            decimal _divider = divider();
+            decimal divider()
+            {
+                return
+                    (decimal)(Math.Pow(1 +
+                    (double)percentRate.ByMonth().PercentNumerical(),
+                    (double)numberOfMonths) - 1);
+            }
+            annuityRateValue = creditAmount * (_percentDoub + (_percentDoub / _divider));
+            return annuityRateValue.Round(4);
+        }
+
+        private decimal balanceOfDebt { get; set; }
+        private decimal percentOnDebt { get; set; }
+        private decimal mainDebt { get; set; }
+        private decimal annuityRate { get; set; }
+
+        private void PercentOnDebt() =>
+            percentOnDebt = (balanceOfDebt * (percentRate.PercentNumerical().ByMonth())).Round(4);
+        private void MainDebt() =>
+            mainDebt = annuityRate - percentOnDebt;
+        private void BalanceOfDebt() =>
+            balanceOfDebt = balanceOfDebt - mainDebt;
+
+
+        public ViewModel ViewData()
+        {
+            PercentOnDebt();
+            MainDebt();
+            BalanceOfDebt();
+            return new ViewModel(
+                balanceOfDebt,
+                percentOnDebt,
+                mainDebt,
+                annuityRate
+                );
+        }
+
     }
+
 }
